fbb67599d39795045dd2f6a55dc9892846448bd0
LiisaJ <34055107+LiisaJ@users.noreply.github.com>
2017-12-05 09:34:19 +0200

    Add files via upload

--- progre.py
+++ progre.py
@@ -1,5 +1,21 @@
-#liitmine
-a=input("palun sisesta a ")
-b=input("palun sisesta b ")
-print("a on " + str(a) + " b on " + str(b))
-print(int(a)+int(b))
+print("Tere!")
+print("Kes sa oled?")
+input("Tere, minu nimi on")
+from random import *
+import math
+a= (randint(1, 10))
+b= (randint(1, 10))
+c= input("Kui palju on " + str(a) + "*" + str(b)+ "?")
+if int(c) == (a*b):
+    print("Tubli!")
+else:
+    print("Sa ei ole just kõige teravam pliiats!")
+print("Järgmine tehe")
+a= (randint(1, 10))
+b= (randint(1, 10))
+c= int(input("sisesta arv, millest võetakse ruutjuur "))
+d= ("Arvuta, kui palju on " + str(a) + "**" + str(b) + "?")
+print(" ruutjuur "+ str(c) + "-st on:", math.sqrt(c))
+
+    
+    
