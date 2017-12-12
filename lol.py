fd59d6890bdda5aceb4695dde59be0fa2ca1056a
LiisaJ <34055107+LiisaJ@users.noreply.github.com>
2017-12-12 09:08:16 +0200

    Add files via upload

---
+++ lol.py
@@ -0,0 +1,20 @@
+print("Tere!")
+print("Kes sa oled?")
+input("Tere, minu nimi on")
+import cmath
+# a = float(input('Sisesta a: '))
+# b = float(input('Sisesta b: '))
+# c = float(input('Sisesta c: '))
+
+d = (b**2) - (4*a*c)
+
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
+
+print("Lahendid on('{0} ja {1}' .format(sol1,sol2))
+      
+      
+
+
+
+
