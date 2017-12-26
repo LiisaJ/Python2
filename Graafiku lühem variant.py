7e5d12f0e353c3ccb9a4e628ce3d99370b4b1c3e
LiisaJ <34055107+LiisaJ@users.noreply.github.com>
2017-12-27 01:57:10 +0200

    Add files via upload

---
+++ Graafiku lühem variant.py
@@ -0,0 +1,10 @@
+from turtle import *
+täht = input("Sisesta täht: ")
+
+if täht == "L" or täht == "l":
+    forward(180)
+    left(180)
+    forward(90)
+    right(90)
+    forward(90)
+    backward(180)
